520147da594d1ca9f2c027f4c295bd416a6b7dad
AlexProverka <alexander.volkov2003.an@gmail.com>
2026-03-15 21:51:09 +0300

    Update ProblemList.tsx

--- frontend/src/components/ProblemList.tsx
+++ frontend/src/components/ProblemList.tsx
@@ -1,1 +1,36 @@
+import React,{useEffect,useState} from "react"
+import {getProblems} from "../api/api"
 
+export default function ProblemList(){
+
+ const [problems,setProblems]=useState<any[]>([])
+
+ useEffect(()=>{
+
+  getProblems().then(res=>{
+   setProblems(res.data)
+  })
+
+ },[])
+
+ return(
+
+  <div>
+
+   <h2>Problems</h2>
+
+   <ul>
+
+    {problems.map(p=>(
+      <li key={p.id}>
+        {p.title}
+      </li>
+    ))}
+
+   </ul>
+
+  </div>
+
+ )
+
+}
