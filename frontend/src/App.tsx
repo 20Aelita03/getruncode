8acb4d0dfca540b7eee4b8d4cb3d0a07ee00fb29
AlexProverka <alexander.volkov2003.an@gmail.com>
2026-03-15 21:50:45 +0300

    Update App.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,1 +1,18 @@
+import React from "react"
+import ProblemList from "./components/ProblemList"
 
+export default function App() {
+
+  return (
+
+    <div>
+
+      <h1>Code Grader</h1>
+
+      <ProblemList/>
+
+    </div>
+
+  )
+
+}
