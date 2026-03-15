88ad38603593571a013ae00134ed5b6ae163e015
AlexProverka <alexander.volkov2003.an@gmail.com>
2026-03-15 21:51:33 +0300

    Update SubmissionResult.tsx

--- frontend/src/components/SubmissionResult.tsx
+++ frontend/src/components/SubmissionResult.tsx
@@ -1,1 +1,28 @@
+import React,{useEffect,useState} from "react"
+import {getSubmission} from "../api/api"
 
+export default function SubmissionResult({id}:{id:number}){
+
+ const [data,setData]=useState<any>(null)
+
+ useEffect(()=>{
+
+  getSubmission(id).then(res=>{
+   setData(res.data)
+  })
+
+ },[id])
+
+ if(!data) return null
+
+ return(
+
+  <div>
+
+   Status: {data.status}
+
+  </div>
+
+ )
+
+}
